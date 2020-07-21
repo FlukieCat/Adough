68cb490db08499d89967fbfe24a797b38194a568
FlukieCat <huabeiyau@gmail.com>
2020-07-21 17:55:41 -0400

    updated calories info

--- client/src/components/dashboard/MineralRadar.js
+++ client/src/components/dashboard/MineralRadar.js
@@ -6,120 +6,108 @@ import { connect } from 'react-redux';
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
-const MineralRadar = ({ nutrition, setting: { setting, loading } }) => {
-    let data = [
-        {
-            id: 'Ca',
-            pct: 0,
-        },
-        {
-            id: 'Fe',
-            pct: 0,
-        },
-        {
-            id: 'Mg',
-            pct: 0,
-        },
-        {
-            id: 'K',
-            pct: 0,
-        },
-        {
-            id: 'Zn',
-            pct: 0,
-        },
-        {
-            id: 'Cu',
-            pct: 0,
-        },
-        {
-            id: 'Se',
-            pct: 0,
-        },
-    ];
-    if (setting.length > 0 && nutrition)
-        data = [
-            {
-                id: 'Ca',
-                pct: loading
-                    ? 0
-                    : Math.min(
+const MineralRadar = ({ nutrition, setting: { setting } }) => {
+    const data =
+        Object.keys(setting).length === 0
+            ? [
+                  {
+                      id: 'Ca',
+                      pct: 0,
+                  },
+                  {
+                      id: 'Fe',
+                      pct: 0,
+                  },
+                  {
+                      id: 'Mg',
+                      pct: 0,
+                  },
+                  {
+                      id: 'K',
+                      pct: 0,
+                  },
+                  {
+                      id: 'Zn',
+                      pct: 0,
+                  },
+                  {
+                      id: 'Cu',
+                      pct: 0,
+                  },
+                  {
+                      id: 'Se',
+                      pct: 0,
+                  },
+              ]
+            : [
+                  {
+                      id: 'Ca',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.calcium / setting.calcium.amount) * 100
                           )
                       ),
-            },
-            {
-                id: 'Fe',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'Fe',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.iron / setting.iron.amount) * 100
                           )
                       ),
-            },
-            {
-                id: 'Mg',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'Mg',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.magnesium / setting.magnesium.amount) *
                                   100
                           )
                       ),
-            },
-            {
-                id: 'K',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'K',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.potassium / setting.potassium.amount) *
                                   100
                           )
                       ),
-            },
-            {
-                id: 'Zn',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'Zn',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.zinc / setting.zinc.amount) * 100
                           )
                       ),
-            },
-            {
-                id: 'Cu',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'Cu',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.copper / setting.copper.amount) * 100
                           )
                       ),
-            },
-            {
-                id: 'Se',
-                pct: loading
-                    ? 0
-                    : Math.min(
+                  },
+                  {
+                      id: 'Se',
+                      pct: Math.min(
                           100,
                           Math.round(
                               (nutrition.selenium / setting.selenium.amount) *
                                   100
                           )
                       ),
-            },
-        ];
+                  },
+              ];
+
     return (
         <ResponsiveRadar
             data={data}
--- client/src/components/dashboard/VitaminRadar.js
+++ client/src/components/dashboard/VitaminRadar.js
@@ -7,124 +7,132 @@ import { connect } from 'react-redux';
 // website examples showcase many properties,
 // you'll often use just a few of them.
 const VitaminRadar = ({ nutrition, setting: { setting, loading } }) => {
-    let data = [
-        {
-            id: 'A',
-            pct: 0,
-        },
-        {
-            id: 'E',
-            pct: 0,
-        },
-        {
-            id: 'C',
-            pct: 0,
-        },
-        {
-            id: 'D',
-            pct: 0,
-        },
-        {
-            id: 'B6',
-            pct: 0,
-        },
-        {
-            id: 'B12',
-            pct: 0,
-        },
-        {
-            id: 'K',
-            pct: 0,
-        },
-    ];
-    if (setting.length > 0 && nutrition)
-        data = [
-            {
-                id: 'A',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminA / setting.vitaminA.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'E',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminE / setting.vitaminE.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'C',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminC / setting.vitaminC.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'D',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminD / setting.vitaminD.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'B6',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminB6 / setting.vitaminB6.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'B12',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminB12 /
-                                  setting.vitaminB12.amount) *
-                                  100
-                          )
-                      ),
-            },
-            {
-                id: 'K',
-                pct: loading
-                    ? 0
-                    : Math.min(
-                          100,
-                          Math.round(
-                              (nutrition.vitaminK / setting.vitaminK.amount) *
-                                  100
-                          )
-                      ),
-            },
-        ];
+    const data =
+        Object.keys(setting).length === 0
+            ? [
+                  {
+                      id: 'A',
+                      pct: 0,
+                  },
+                  {
+                      id: 'E',
+                      pct: 0,
+                  },
+                  {
+                      id: 'C',
+                      pct: 0,
+                  },
+                  {
+                      id: 'D',
+                      pct: 0,
+                  },
+                  {
+                      id: 'B6',
+                      pct: 0,
+                  },
+                  {
+                      id: 'B12',
+                      pct: 0,
+                  },
+                  {
+                      id: 'K',
+                      pct: 0,
+                  },
+              ]
+            : [
+                  {
+                      id: 'A',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminA /
+                                        setting.vitaminA.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'E',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminE /
+                                        setting.vitaminE.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'C',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminC /
+                                        setting.vitaminC.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'D',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminD /
+                                        setting.vitaminD.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'B6',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminB6 /
+                                        setting.vitaminB6.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'B12',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminB12 /
+                                        setting.vitaminB12.amount) *
+                                        100
+                                )
+                            ),
+                  },
+                  {
+                      id: 'K',
+                      pct: loading
+                          ? 0
+                          : Math.min(
+                                100,
+                                Math.round(
+                                    (nutrition.vitaminK /
+                                        setting.vitaminK.amount) *
+                                        100
+                                )
+                            ),
+                  },
+              ];
+
     return (
         <ResponsiveRadar
             data={data}
